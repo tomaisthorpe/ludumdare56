dacb2e3aa0614b09dc47ec6351e579d4b70d94c6
Tom Aisthorpe <tomaisthorpe@gmail.com>
2024-10-05 13:17:10 +0100

    feat: add styling to stats

--- src/ColonyStats.tsx
+++ src/ColonyStats.tsx
@@ -1,14 +1,36 @@
 import styled from "styled-components";
 import { useGameContext } from "@tedengine/ted";
+import config from "./game/config";
+
 const Container = styled.div`
   position: absolute;
   bottom: 0;
   right: 0;
-  background-color: rgba(0, 0, 0, 0.5);
   color: white;
-  font-family: monospace;
   text-align: left;
   padding: 15px 10px;
+`;
+
+const Group = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 300px;
+  font-size: 22px;
+  border: 2px solid ${config.palette.persianOrange};
+  background-color: ${config.palette.earthYellow}cc;
+  border-radius: 10px;
+  padding: 5px 10px;
+`;
+
+const GroupLabel = styled.div`
+  font-weight: bold;
+  font-size: 24px;
+  color: ${config.palette.onyx};
+  margin-top: 20px;
+`;
+
+const Stat = styled.div`
+  width: 50%;
 `;
 
 export function ColonyStats() {
@@ -17,12 +39,21 @@ export function ColonyStats() {
   if (!colony) return null;
   return (
     <Container>
-      <div>Bees: {colony.numBees}</div>
-      <div>Honey: {colony.honeyReserves}</div>
-      <div>Honey Consumption: {colony.honeyConsumption.toFixed(1)}</div>
-      <div>
-        How Long Will Honey Last: {colony.howLongWillHoneyLast.toFixed(0)}
-      </div>
+      <GroupLabel>Colony</GroupLabel>
+      <Group>
+        <Stat>Bees: {colony.numBees}</Stat>
+        <Stat>Brood: 0</Stat>
+        <Stat>Laying Rate: 2</Stat>
+      </Group>
+
+      <GroupLabel>Honey</GroupLabel>
+      <Group>
+        <div>Honey: {colony.honeyReserves}</div>
+        <div>Honey Consumption: {colony.honeyConsumption.toFixed(1)}</div>
+        <div>
+          How Long Will Honey Last: {colony.howLongWillHoneyLast.toFixed(0)}
+        </div>
+      </Group>
     </Container>
   );
 }
